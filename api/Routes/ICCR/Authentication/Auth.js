2ba2ddceba005a9d33b975c04f57768077820928
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2022-08-21 13:40:11 +0530

    Backend and Database Connection successful

---
+++ api/Routes/ICCR/Authentication/Auth.js
@@ -0,0 +1,17 @@
+const router = require("express").Router()
+const dotenv = require("dotenv")
+dotenv.config()
+const jwt = require('jsonwebtoken')
+
+
+
+router.post('/login', async (req, res) => {
+
+    
+
+})
+
+
+
+
+module.exports = router
---
+++ api/index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const cors = require("cors");
+
+
+// res.header( "Access-Control-Allow-Origin" );
+
+const authRoute = require("../api/Routes/ICCR/Authentication/Auth")
+
+
+dotenv.config();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB COnnection Succesful"));
+  }
+
+
+app.use(express.json())
+
+app.use("/auth" , authRoute)
+
+
+app.listen( 8001 , ()=>{
+    console.log( "Backend Server is Running" );
+})
