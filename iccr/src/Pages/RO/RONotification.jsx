48462419e97b7f0b6008bdc21c31464fab97dbec
Neha-Chopade <111578669+Neha-Chopade@users.noreply.github.com>
2022-08-26 09:35:53 +0530

    RO Dashboard

---
+++ iccr/src/Pages/RO/RONotification.jsx
@@ -0,0 +1,10 @@
+import RONavbar from "./RONavbar";
+import React from "react";
+
+export default function RONotification(){
+    <div className="RONotification">
+            <RONavbar/>
+            
+    </div>
+    
+}
